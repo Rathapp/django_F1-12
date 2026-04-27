01b6902d2458286f0488def872bd2566f4bc70cc
Rathapp <povrathapp@gmail.com>
2026-04-27 17:50:38 +0700

    first commit

---
+++ patients/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'patients/home.html')
+
+
+def about(request):
+    return render(request,'patients/about.html')
+
+def register(request):
+    return render(request,'patients/register.html')
+
